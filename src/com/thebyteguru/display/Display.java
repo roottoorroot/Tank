34bd9ddccdfa74bf2245d907f6f2a617be13c6c8
roottoorroot <ilyafulleveline@gmail.com>
2016-02-29 13:40:33 +0400

    Изменяем фон поля и рисуем движушийся шар;

--- src/com/thebyteguru/display/Display.java
+++ src/com/thebyteguru/display/Display.java
@@ -3,16 +3,28 @@ package com.thebyteguru.display;
 import com.sun.java.swing.plaf.windows.resources.windows;
 import javax.swing.JFrame;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public abstract class Display {
-    private static boolean kreated = false; //Контроль создания окна;
+    private static boolean created = false; //Контроль создания окна;
     private static JFrame window; //Наше окно из библиотеки;
     private static Canvas content; //Свободный член, нужен для описания характеристик окна;
+    private static BufferedImage buffer; //Наша картинка; 
+    private static int[] bufferData; //В этот массив мы запихаем наше изображение;
+    private static Graphics bufferGraphics; //Для рисования и отображения получившихся картинок;
+    private static int clearColor; //Цвет для закрашивания; нашего поля;
+    //temp
+    private static float delta = 0; //Для тестирования движения;
+    //temp end
     
-    public static void create(int width, int height, String title){
+    public static void create(int width, int height, String title, int _clearColor){
         //Если окно уже создано, не зоздаём еще одно, завершаем функцию;
-        if(kreated){ 
+        if(created){ 
             return;
         } 
         window = new JFrame(title); //Создаём окно и передаём название окну;
@@ -26,6 +38,30 @@ public abstract class Display {
         window.setVisible(true); //Обязательно ставим видимость окна;
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Теперь наше окно полностью закрыват программу при нажатии на крестик окна;
         window.setLocationRelativeTo(null); //Позиция созданного окна по дифолту;
+        
+        buffer = new BufferedImage(width, height,BufferedImage.TYPE_INT_ARGB); //Для хранения цвета выбираем тип: TYPE_INT_ARGB;
+        bufferData = ((DataBufferInt)buffer.getRaster().getDataBuffer()).getData(); //Данна конструкция нужна для перехвата массива с цветами и дальнейшего издевательства с ним;
+        bufferGraphics = buffer.getGraphics(); //Достаем все параметры и цвета и распооложение;
+        clearColor = _clearColor; //Инициализируем цвет заполнения;
+        
+        created = true;
     }
+    
+    public static void clear(){
+        Arrays.fill(bufferData,clearColor); //Функция заполняет весь переданный ей массив выбранным значением, в нашем случае, это цвет clearColor;
+    }
+    
+    public static void render(){
+        bufferGraphics.setColor(new Color(0xff0000ff)); //Задаем цвет объекта, который хотим передать;
+        bufferGraphics.fillOval((int)(350 + Math.sin(delta) * 200), 250, 100, 100); //Рисуем круг;
+        
+        delta += 0.02f; //Для движения Шарика;
+    }
+    
+    public static void swapBuffers(){
+        Graphics g = content.getGraphics(); //Вытаскиваем всё то что есть у нас сейчас в Канвасе через контент;
+        g.drawImage(buffer, 0, 0, null); //Запихиваем в графикс текущий объект в буфере: Шарик;
+    }
+    
     
 }
